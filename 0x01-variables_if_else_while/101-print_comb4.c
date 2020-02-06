2301cfa86794705848010865ea0b1eb325fc41cc
zacwoll <zacwoll@gmail.com>
2020-02-06 23:19:06 +0000

    added 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry Point
+  * Description: Prints the alphabet in lowercase, followed by a new line
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (b > a && c > b)
+				{
+					putchar(48 + a);
+					putchar(48 + b);
+					putchar(48 + c);
+					if (a != 7 || b != 8 || c != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

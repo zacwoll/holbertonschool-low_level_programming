72c1d1b02265738d782c0d8a49b9e51c49b7a9ec
zacwoll <zacwoll@gmail.com>
2020-02-06 01:26:10 +0000

    Added 102

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: Prints interesting quote, followed by a new line
+  * Return: Always 1 (Failure)
+  */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

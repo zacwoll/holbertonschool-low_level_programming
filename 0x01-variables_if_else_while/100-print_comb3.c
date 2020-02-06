4ae242a5bdbcca0e8209b5290590427cb639268f
zacwoll <zacwoll@gmail.com>
2020-02-06 21:44:55 +0000

    added 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - Entry Point
+  * Description: Prints the alphabet in lowercase, followed by a new line
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (b > a)
+			{
+				putchar(48 + a);
+				putchar(48 + b);
+				if (a != 8 || b != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

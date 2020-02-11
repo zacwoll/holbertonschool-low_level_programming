de542148b0c1ae9922c724009f879b040980d07a
zacwoll <zacwoll@gmail.com>
2020-02-11 20:42:40 +0000

    Properly formatted 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,10 +16,23 @@ void print_times_table(int n)
 			for (j = 0; j < n; j++)
 			{
 				product = i * j;
-				if (product > 9)
+				if (product > 99)
+				{
+					_putchar(product / 100 + '0');
+					_putchar(product / 10 % 10 + '0');
+				}
+				else if (product > 9)
+				{
+					_putchar(' ');
 					_putchar(product / 10 + '0');
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(product % 10 + '0');
-				if (j < n)
+				if (j < n - 1)
 				{
 					_putchar(',');
 					_putchar(' ');

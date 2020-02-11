86f9732e9c03180dba8789dcce637df173e1dea9
zacwoll <zacwoll@gmail.com>
2020-02-11 22:35:12 +0000

    First draft of 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
   * print_fib_98 - prints the first 98 Fibonacci numbers starting at 1, 2
   * Return: void
   */
-void print_fib_98(void)
+int main(void)
 {
 	int i = 2;
 	unsigned long a = 1, b = 2, term = 0, a_2 = 0, b_2 = 0, term_2 = 0;
@@ -29,4 +29,5 @@ void print_fib_98(void)
 		a = b;
 		b = term;
 	}
+	return 0;
 }

6c921829549a6f7bffc114ac39e46a87b6d5afab
zacwoll <zacwoll@gmail.com>
2020-02-11 22:01:55 +0000

    101 has a main now

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "holberton.h"
 /**
   * print_sum - prints the sum of the multiples of 3 and 5 up to 1024
   * Return: void
   */
-void print_sum(void)
+int main(void)
 {
 	int max = 1024;
 	int i, total;
@@ -15,4 +14,6 @@ void print_sum(void)
 			total += i;
 	}
 	printf("%d\n", total);
+
+	return (0);
 }

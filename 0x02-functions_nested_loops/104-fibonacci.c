4d158d09efab1841d9fbf7314d445e37605f95af
zacwoll <zacwoll@gmail.com>
2020-02-11 23:39:40 +0000

    Got 104 to compile with correct outputgit add 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,21 +5,23 @@
   */
 int main(void)
 {
-	int i = 2;
+	int i = 3, is_big = 0;
 	unsigned long a = 1, b = 2, term = 0, a_2 = 0, b_2 = 0, term_2 = 0;
 	unsigned long cutoff = 10000000000000000;
 
 	printf("%lu, %lu", a, b);
-	for (; i < 99; i++)
+	for (; i < 100; i++)
 	{
-		if (i < 98)
-			printf(", ");
+		printf(", ");
 		term = a + b;
 		if (term > cutoff)
+			is_big = 1;
+		if (is_big)
 		{
 			term_2 = term / cutoff + a_2 + b_2;
 			term = term % cutoff;
-			printf("%lu%lu", term_2, term);
+			printf("%lu", term_2);
+			printf("%016lu", term);
 			a_2 = b_2;
 			b_2 = term_2;
 		}

82c8c5620e7a225de347602b496ef4cab8ae3107
zacwoll <zacwoll@gmail.com>
2020-02-11 19:05:23 +0000

    made 11 betty-compliant

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,13 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for(; n > 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 	}
 	else
 	{
-		for(; n < 98; n++)
+		for (; n < 98; n++)
 			printf("%d, ", n);
 	}
-	printf("%d\n", n);
+	printf ("%d\n", n);
 }

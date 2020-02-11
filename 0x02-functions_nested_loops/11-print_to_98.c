4c76d64f4a1f045df231c92a832430e64cd518c7
zacwoll <zacwoll@gmail.com>
2020-02-11 18:43:52 +0000

    11 formatted

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,21 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			if (n == 98)
+			if (n != 98)
 				printf(", ");
 		}
 	}
 	if (n <= 98)
 	{
-		for (n; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			if (n == 98)
+			if (n != 98)
 				printf(", ");
 		}
 	}
+	printf("\n");
 }

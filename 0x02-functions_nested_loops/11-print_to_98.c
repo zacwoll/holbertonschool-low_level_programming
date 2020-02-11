f23797bc00484a3f1bd05474244623f4858c9691
zacwoll <zacwoll@gmail.com>
2020-02-11 19:02:50 +0000

    refactored 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,13 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-		}
+		for(; n > 98; n--)
+			printf("%d, ", n);
 	}
-	if (n <= 98)
+	else
 	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(", ");
-		}
+		for(; n < 98; n++)
+			printf("%d, ", n);
 	}
-	printf("\n");
+	printf("%d\n", n);
 }

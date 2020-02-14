9757f25e200ec139f0e60ef19d3f5095732a2c70
zacwoll <zacwoll@gmail.com>
2020-02-14 00:31:26 +0000

    added the 10x and the final 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,15 @@
   */
 void more_numbers(void)
 {
-	int i;
-
-	for (i = 0; i < 14; i++)
-		if (i / 10 > 0)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
-	_putchar('\n');
+	int i, k;
+	for (k = 0; k < 10; k++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i / 10 > 0)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }

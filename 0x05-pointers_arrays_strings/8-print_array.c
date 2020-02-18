90f52d92b44f5c75f786ebe612c613691bd937d2
zacwoll <zacwoll@gmail.com>
2020-02-18 21:14:18 +0000

    Removed Tester

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,16 +15,3 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 		printf("%d\n", e[i]);
 }
-
-int main(void)
-{
-	int array[5];
-
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
-    return (0);
-}	

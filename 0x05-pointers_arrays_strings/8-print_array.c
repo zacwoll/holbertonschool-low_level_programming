d76ba9870a227b2f2d8c95eec9b54a6d8d14d2f7
zacwoll <zacwoll@gmail.com>
2020-02-18 21:22:34 +0000

    8 now compiles

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
   * print_array - prints an array, followed by a new line
   * @a : pointer to array
@@ -10,7 +11,7 @@ void print_array(int *a, int n)
 {
 	int *e = a;
 
-	int i, j;
+	int i;
 
 	for (i = 0; i < n; i++)
 		printf("%d\n", e[i]);

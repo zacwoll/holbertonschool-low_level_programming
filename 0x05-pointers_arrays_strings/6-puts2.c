b4e2000dcf98e8d627379559b8ac46721ab30832
zacwoll <zacwoll@gmail.com>
2020-02-18 22:44:27 +0000

    covered the out of bounds

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,11 +5,11 @@
   * @str : string to be printed
   * Return: void
   */
-/* str points to "101234567890806" */
+
 void puts2(char *str)
 {
 	_putchar(*str);
-	while (*(str + 1) && *(str + 2))
+	while (!(*(str + 1)) || !(*(str + 2)))
 	{
 		str += 2;
 		_putchar(*str);

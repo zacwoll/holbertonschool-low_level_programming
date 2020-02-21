e64ff8391901b1dde94d633ab28eaab6ec38295e
zacwoll <zacwoll@gmail.com>
2020-02-21 01:05:48 +0000

    copied from 0x04

---
+++ 0x06-pointers_arrays_strings/100-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * print_number - Entry Point
+  * @n: integer to be printed
+  * Return: 0 (Success)
+  */
+void print_number(int n)
+{
+	unsigned int num;
+	/*
+	 * if number is negative, print -1 then take abs(n)
+	 * recursively call this function to print each digit
+	 * or, print the front digit and then pass the rest to
+	 * the next iteration
+	 */
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+		num = n;
+
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
+}

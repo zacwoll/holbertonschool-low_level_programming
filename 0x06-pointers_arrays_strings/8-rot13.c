d76ec53edafd66ba137330009fac7679ea0781da
zacwoll <zacwoll@gmail.com>
2020-02-21 00:28:12 +0000

    Matched expected output

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  * @c: character pointer to source
+  * Return: character pointer to source
+  */
+char *rot13(char *c)
+{
+	char *p = c;
+	char *alpha = "abcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "nopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+
+	while (*c)
+	{
+		for (i = 0; alpha[i]; i++)
+		{
+			if (*c == alpha[i])
+			{
+				*c = rot13[i];
+				break;
+			}
+			else if (*c == alpha[i] - 'a' + 'A')
+			{
+				*c = rot13[i] - 'a' + 'A';
+				break;
+			}
+		}
+		c++;
+	}
+
+	return p;
+}

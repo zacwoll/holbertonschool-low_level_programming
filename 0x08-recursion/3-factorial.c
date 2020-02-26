4b7cac1aba45899163d27fd52216f4d0e729e9b5
vagrant <vagrant@precise32.(none)>
2020-02-26 23:30:07 +0000

    forgot to enclose answer in parentheses

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,10 @@
 
 int factorial(int n)
 {
-	if (n == 1 || n == 0)
-		return (1);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
+	else if (n == 0)
+		return (1);
 	else
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 }

199ca3c12bfb3606dc21cf2ba87b8b0a440c6c5f
vagrant <vagrant@precise32.(none)>
2020-02-26 23:14:18 +0000

    just rewrote the program to pass checks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
   */
 void _puts_recursion(char *s)
 {
+	if (!*s)
+		return;
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(++s);
+		_puts_recursion(s + 1);
+		if (*(s + 1) == '\0')
+			_putchar('\n');
 	}
-	else
-		_putchar('\n');
 }

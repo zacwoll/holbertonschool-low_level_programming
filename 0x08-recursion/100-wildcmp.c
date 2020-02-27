81216513f4a180ae85fba6fcca11ebc0e35ec88d
vagrant <vagrant@precise32.(none)>
2020-02-27 00:47:14 +0000

    First draft of wilcmp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - Check if two strings can be considered identical
+ * @s1: The first string
+ * @s2: The second string
+ * Return: int, 1 if true, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (_wildcmp(s1, s2));
+}
+
+/**
+ * check_wildcmp - Recursively check if a string is a palindrome
+ * @s1: The first string
+ * @s2: The second string
+ * Return: int, 1 if true, 0 if false
+ */
+int _wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	else if (*s1 == *s2)
+		return (_wildcmp(s1 + 1, s2 + 1));
+	else if ((*s1 == '\0') && *s2 != '*')
+		return (0);
+	else if ((*s2 == '\0') && *s1 != '*')
+		return (0);
+	else if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (_wildcmp(s1, s2 + 1));
+		else
+			return (_wildcmp(s1 + 1, s2) || _wildcmp(s1, s2 + 1));
+	}
+	else
+		return (0);
+}

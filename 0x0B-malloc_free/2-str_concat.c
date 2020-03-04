1a237a84c5a8707c686b399394941e46907a9e5f
zacwoll <zacwoll@gmail.com>
2020-03-04 18:38:21 +0000

    covered the double null case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,12 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *p;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{}
 	for (j = 0; s2[j]; j++)

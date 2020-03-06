cc93acf50300477dd8364b7232ff01ad8d5e577e
zacwoll <zacwoll@gmail.com>
2020-03-06 00:36:10 +0000

    removed unnecessary minimum

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,18 +10,25 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	/* Declarations */
-	unsigned int min = new_size, i;
-	void *new_ptr;
-	/* new minimum */
-	if (old_size < min)
-		min = old_size;
-	else if (old_size == min)
+	unsigned int i;
+	char *new_ptr;
+
+	if (new_size == old_size)
 		return (ptr);
-	new_ptr = malloc(new_size);
-	for (i = 0; i <= min; i++)
+
+	if (new_size == 0 && ptr != NULL)
 	{
-		*((char *)new_ptr + i) = *((char *)ptr + i);
+		free(ptr);
+		return (NULL);
+	}
+	
+	new_ptr = malloc(sizeof(char) * new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr != NULL)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = *((char *)ptr + i);
 	}
 	free(ptr);
 	return (new_ptr);

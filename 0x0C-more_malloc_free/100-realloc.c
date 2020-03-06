7bf04917ff5e4724bf0663c2e24c928b76131efc
zacwoll <zacwoll@gmail.com>
2020-03-06 00:37:32 +0000

    Trailing whitespace

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	
+
 	new_ptr = malloc(sizeof(char) * new_size);
 	if (new_ptr == NULL)
 		return (NULL);

4b8d58926de36f6886ef75c56376e37ca1e268e4
zacwoll <zacwoll@gmail.com>
2020-03-14 19:26:08 +0000

    fixed compilation error

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,10 +12,10 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 /* structs */
-struct fm_t
+typedef struct fm
 {
 	char *fm;
 	void (*p)();
-};
+} fm_t;
 
 #endif /* VARIADIC_FUNCTIONS_H */

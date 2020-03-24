bfb2205a024a78892131bedbd72f251d2c3656f3
zacwoll <zacwoll@gmail.com>
2020-03-24 22:22:21 +0000

    Correct Output, Compiles

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -15,10 +15,11 @@ listint_t *find_listint_loop(listint_t *head)
 		if (head < head->next)
 		{
 			head = head->next;
-			printf("-> [%p] %d\n", (void *) head, head->n);
 			return (head);
 		}
 		else
 			head = head->next;
 	}
+
+	return (NULL);
 }

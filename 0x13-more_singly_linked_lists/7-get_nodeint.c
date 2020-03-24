e644f6e1f4e2c033532d2bdd3f89aec61c42b13c
zacwoll <zacwoll@gmail.com>
2020-03-24 18:12:08 +0000

    added another null check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,14 +8,16 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	/* listint_t *ret_node; */
 	unsigned int i;
 
 	if (!head)
 		return (NULL);
 	for (i = 0; i < index; i++)
 	{
-		head = head->next;
+		if (head->next)
+			head = head->next;
+		else
+			return (NULL);
 	}
 
 	return (head);

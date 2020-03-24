3bbb45f33ac8fca050ee9a92f160eb9e8a4687e1
zacwoll <zacwoll@gmail.com>
2020-03-24 22:06:45 +0000

    literally reversed the if else because nothing makes sense

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,14 +14,14 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *) head, head->n);
 		count++;
 
-		if (head < head->next)
+		if (head > head->next)
+			head = head->next;
+		else
 		{
 			head = head->next;
 			printf("-> [%p] %d\n", (void *) head, head->n);
 			break;
 		}
-		else
-			head = head->next;
 	}
 
 	return (count);

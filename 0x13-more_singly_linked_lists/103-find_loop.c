a6d67ca71491c3e2e0fd01c6a7a8f0be43b2bbc1
zacwoll <zacwoll@gmail.com>
2020-03-25 00:17:09 +0000

    Googled Problem, came back with Floyd's Tortoise and Hare

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,25 +1,33 @@
 #include "lists.h"
 
 /**
-  * find_listint_loop - finds where it loops in the list if it does
+  * find_listint_loop - Floyd's Tortoise and Hare, finds loop in list
   * @head: head of the list
   * Return: listint_t address of loop
   */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *back, *front;
+
 	if (!head)
 		return (NULL);
 
-	while (head)
+	back = head
+	front = head;
+	while (front->next && front->next->next)
 	{
-		if (head < head->next)
+		front = front->next->next;
+		back = back->next;
+		if (front == back)
 		{
-			head = head->next;
-			return (head);
+			back = head;
+			while (back != front)
+			{
+				back = back->next;
+				front = front->next;
+			}
+			return (front);
 		}
-		else
-			head = head->next;
 	}
-
 	return (NULL);
 }

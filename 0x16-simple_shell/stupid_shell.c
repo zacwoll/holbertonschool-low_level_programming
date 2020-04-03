4958eeeab3786975855297c65fd4491ba93c5d57
zacwoll <zacwoll@gmail.com>
2020-04-03 17:12:02 +0000

    IT WORKS

--- 0x16-simple_shell/stupid_shell.c
+++ 0x16-simple_shell/stupid_shell.c
@@ -8,28 +8,48 @@
 int main(int ac, char **av)
 {
 	char *line_buf = NULL;
+	char *temp = NULL;
 	size_t line_buf_size = 0;
 	int line_count = 0;
 	ssize_t line_size;
 	pid_t child_pid;
 	int status;
 	char **args;
-	int argcount = 0;
+	int argcount = 1;
 	char *token;
-	
+	int i;
 		/* shell prompt and argument acception*/
 		printf("ShiP$ ");
 		line_size = getline(&line_buf, &line_buf_size, stdin);
 		
-		do
-		{
-			token = strtok(NULL, line_buf);
-			memcpy(args[argcount], token, strlen(token) + 1);
-			argcount++;
-		} while (token != NULL);
+		line_buf[strlen(line_buf) - 1] = '\0';
 
+		/* malloc for the number of strings */
+		temp = strdup(line_buf);
+		token = strtok(temp, " ");
+		while (token != NULL)
+		{
+			argcount++;
+			token = strtok(NULL, temp);
+		}
+
+		args = malloc(argcount * sizeof(char *));
+
+		/* set those strings into our array */
+		argcount = 0;
+		token = strtok(line_buf, " ");
+		while (token != NULL)
+		{
+			args[argcount] = token;
+			argcount++;
+			token = strtok(NULL, line_buf);
+			printf("%s==", token);
+		}
+
+		/* not used in this example */
 		(void)ac;
 
+		/* executes our command */
 		child_pid = fork();
 		if (child_pid == -1)
 		{

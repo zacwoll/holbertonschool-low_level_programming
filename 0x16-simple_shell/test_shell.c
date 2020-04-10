9e11212f73bfbc592a747751352b10f98823a9f6
zacwoll <zacwoll@gmail.com>
2020-04-10 19:02:47 +0000

    starting to look like a shell..

--- 0x16-simple_shell/test_shell.c
+++ 0x16-simple_shell/test_shell.c
@@ -8,34 +8,35 @@ int main(void)
 	ssize_t getline_size;
 	char **argv, **env, *path_to_file, *line = NULL;
 	struct stat st;
-	char *built_ins[] = {"cd", "setenv", "unsetenv", "exit"};
+
 	env = _initenv();
 
-	printf("ShiP$ ");
-	getline_size = getline(&line, &line_size, stdin);
-	line[getline_size - 1] = '\0';
-	printf("line_size: %li\n", getline_size);
-
-	rem_comments(line);
-	argv = get_tokens(line, " ");
-
-	cmd_handler(argv, &env);
-	
-	/*
-	child_pid = fork();
-	if (child_pid == -1)
+	while (1)
 	{
-		perror("Error:");
-	}
-	if (child_pid == 0)
-	{
-		if (execve(path_to_file, argv, env) == -1)
+		printf("ShiP$ ");
+		getline_size = getline(&line, &line_size, stdin);
+		line[getline_size - 1] = '\0';
+		printf("line_size: %li\n", getline_size);
+
+		rem_comments(line);
+		argv = get_tokens(line, " ");
+
+		if (!strcmp(argv[0], "exit"))
+		{
+			/* handle exit */
+		}
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
 			perror("Error:");
-		return (0);
+		}
+		if (child_pid == 0)
+		{
+			cmd_handler(argv, &env);
+		}
+		wait(&status);
 	}
-
-	wait(&status);
-	*/
 	free(line);
 	free(argv);
 

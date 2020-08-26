bffc73d5bf9122c6abea2ca6046e1f29056e0f29
zacwoll <zacwoll@gmail.com>
2020-08-26 20:23:30 +0000

    fixed out of bounds error and logical typo

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -10,6 +10,7 @@
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1, mid;
+	int breaknow = 0;
 
 	if (!array)
 		return (-1);
@@ -18,12 +19,19 @@ int advanced_binary(int *array, size_t size, int value)
 		for (mid = low, printf("Searching in array: "); mid <= high; mid++)
 			printf("%d%s", array[mid], mid == high ? "\n" : ", ");
 		mid = low + (high - low) / 2;
-		if (low == high && low != value)
-			return (-1);
-		if (array[mid] > value || array[mid - 1] == value)
-			high = mid;
-		else if (array[mid] < value)
+
+		if (low == mid)
+		{
+			if (array[low] == value)
+				return (low);
+			else
+				return (-1);
+		}
+
+		if (array[mid] < value)
 			low = mid + 1;
+		else if (array[mid] > value || array[mid - 1] == value)
+			high = mid;
 		else
 			return (mid);
 	}

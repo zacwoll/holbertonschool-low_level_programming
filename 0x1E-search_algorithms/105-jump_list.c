5f2a4a3c8516b94523d6ea2c1fb4fa09d855ca40
zacwoll <zacwoll@gmail.com>
2020-08-26 22:01:49 +0000

    105-jump_list and included all the list items in the main

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_list - jump search in a singly linked list
+ * @list: the list to  be searched
+ * @size: size of the list
+ * @value: key to search for
+ * Return: listint_t *ptr to node or NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t i = 0, j = sqrt(size), j_prev = 0;
+	listint_t *list_prev = list;
+
+	if (!list)
+		return (NULL);
+
+	while (list->n < value)
+	{
+		for (j_prev = i, list_prev = list; list->next && i < j_prev + j; i++)
+			list = list->next;
+		printf("Value checked array[%lu] = [%d]\n", i, list->n);
+		if (!list->next)
+			break;
+	}
+
+	printf("Value found between indexes [%lu] = [%lu]\n", j_prev, i);
+	
+	for (i = j_prev, list = list_prev; list->next && list->n < value; i++)
+	{
+		list = list->next;
+		printf("Value checked array[%lu] = [%d]\n", i, list->n);
+	}
+
+	if (list->n == value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, list->n);
+		return (list);
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* structs */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -12,4 +30,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif

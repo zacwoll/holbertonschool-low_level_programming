91c9b075f790326a07c2687abe07ed1695a062d5
Zac Woll <zacwoll@gmail.com>
2022-01-31 16:18:48 -0600

    Structured old code

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  * positive_or_negative - determines a number's polarity
+  * Return: 0 (Success)
+  */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+}

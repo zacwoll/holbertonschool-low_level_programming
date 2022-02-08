57e3deef142f53c6c35ee5fdc2bab8626c0557cb
zacwoll <zacwoll@gmail.com>
2022-02-08 10:57:10 -0600

    Began updating the old C project

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
   * print_array - prints an array, followed by a new line
   * @a : pointer to array
   * @n : size of array
   * Return: void
   */
-
 void print_array(int *a, int n)
 {
-	int *e = a;
 	int i;
 
 	for (i = 0; i < n; i++)
-		if (i >= 0)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
 			printf(", ");
-		printf("%d", e[i]);
+		}
+	}
 	printf("\n");
 }

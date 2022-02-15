bfe097f0badb3af80862716982105f6660e770c1
zacwoll <zacwoll@gmail.com>
2022-02-15 13:41:29 -0600

    took out strtol put in new code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,29 +8,33 @@
   * @argv: vector of arguments
   * Return: int
   */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int sum = 0, i, part;
-	char *err;
+	int sum = 0, i, j;
 
+	// arguments < 2 means no result
 	if (argc < 2)
 	{
 		printf("%d\n", sum);
 		return (0);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
-		part = strtol(argv[i], &err, 10);
-		if (*err == '\0')
+		// loop for each character in each argv
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += part;
+			// check if character is not digit
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		// Assuming no errors, sum the arguments
+		sum += atoi(argv[i]);
 	}
+	
 	printf("%d\n", sum);
 	return (0);
 }
